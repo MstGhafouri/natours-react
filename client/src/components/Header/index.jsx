5f2ed94e24f3f07c33d217bb4b73b54b9abc8543
Mostafa Ghafouri <mstghafouri@gmail.com>
2020-07-31 20:52:26 +0430

    Add some route guards in header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,39 +1,47 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { Menu, Image, Icon } from 'semantic-ui-react';
 
 import SearchBar from './SearchBar';
 import logo from '../../assets/img/icons/logo-white.png';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <Menu className="navigation" borderless>
-        <Menu.Item
-          as={NavLink}
-          exact
-          to="/"
-          name="AllTours"
-          className="animated-item"
-        >
-          <Image src={logo} alt="Natours" className="navigation__logo" />
-        </Menu.Item>
+const Header = () => {
+  const pathname = useLocation().pathname;
+
+  console.log(pathname);
+
+  return (
+    <Menu className="navigation" borderless>
+      <Menu.Item
+        as={NavLink}
+        exact
+        to="/"
+        name="AllTours"
+        className="animated-item"
+      >
+        <Image src={logo} alt="Natours" className="navigation__logo" />
+      </Menu.Item>
+      {pathname === '/' && (
         <Menu.Item>
           <SearchBar />
         </Menu.Item>
-        <Menu.Menu position="right">
+      )}
+      <Menu.Menu position="right">
+        {pathname !== '/login' && (
           <Menu.Item as={NavLink} exact className="animated-item" to="/login">
             <Icon name="sign-in" />
             Log in
           </Menu.Item>
+        )}
+        {pathname !== '/signup' && (
           <Menu.Item as={NavLink} exact className="animated-item" to="/signup">
             <Icon name="plus" />
             Sign up
           </Menu.Item>
-        </Menu.Menu>
-      </Menu>
-    );
-  }
-}
+        )}
+      </Menu.Menu>
+    </Menu>
+  );
+};
 
 export default Header;
