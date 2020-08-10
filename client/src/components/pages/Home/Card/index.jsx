a1c1d058cf118942cc2ab28ceb5921cce124edb5
Mostafa Ghafouri <mstghafouri@gmail.com>
2020-08-10 19:26:36 +0430

    Add styled-component package

--- client/src/components/pages/Home/Card/index.jsx
+++ client/src/components/pages/Home/Card/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Icon, Grid, Label } from 'semantic-ui-react';
+import styled from 'styled-components';
 
 import CustomBtn from '../../../utils/CustomBtn';
-import imageCover from '../../../../assets/img/tours/tour-1-cover.jpg';
 
 const colorConfig = {
   summer: ['126, 213, 111', '40, 180, 133', 'green'],
@@ -10,19 +10,49 @@ const colorConfig = {
   fall: ['255, 185, 0', '255, 119, 48', 'orange']
 };
 
-const Card = ({ season }) => {
+const CardPictureOverlay = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-blend-mode: screen;
+  background-size: cover;
+  background-image: linear-gradient(
+      to bottom right,
+      rgb(${props => colorConfig[props.season][0]}),
+      rgb(${props => colorConfig[props.season][1]})
+    ),
+    url(${props => props.imgUrl});
+`;
+
+// ../../../../assets/img/tours/${imageCover}
+
+const Card = props => {
+  const {
+    season,
+    name,
+    difficulty,
+    duration,
+    summary,
+    price,
+    maxGroupSize,
+    ratingsAverage,
+    ratingsQuantity,
+    locations,
+    startDates,
+    startLocation,
+    slug,
+    imageCover
+  } = props;
+
   return (
     <div className="card">
       <div className="card__header">
         <div className="card__picture">
-          <div
-            className="card__picture-overlay"
-            style={{
-              backgroundImage: `linear-gradient(to bottom right, rgb(${colorConfig[season][0]}), rgb(${colorConfig[season][1]})), url(${imageCover})`
-            }}
-          >
-            &nbsp;
-          </div>
+          {/* <div className="card__picture-overlay">&nbsp;</div> */}
+          <CardPictureOverlay
+            season={season}
+            imgUrl={`${process.env.PUBLIC_URL}/img/tours/${imageCover}`}
+          ></CardPictureOverlay>
         </div>
 
         <h3 className="heading-tertiary">
@@ -31,7 +61,7 @@ const Card = ({ season }) => {
               backgroundImage: `linear-gradient(to bottom right, rgba(${colorConfig[season][0]}, 0.8), rgba(${colorConfig[season][1]}, 0.8))`
             }}
           >
-            The Forest Hiker
+            {name}
           </span>
         </h3>
       </div>
@@ -40,15 +70,13 @@ const Card = ({ season }) => {
         <Grid>
           <Grid.Row>
             <Grid.Column>
-              <h4 className="card__sub-heading">Easy 5-day tour</h4>
+              <h4 className="card__sub-heading">{`${difficulty} ${duration}-day tour`}</h4>
             </Grid.Column>
           </Grid.Row>
 
           <Grid.Row>
             <Grid.Column>
-              <p className="card__text">
-                Breathtaking hike through the Canadian Banff National Park
-              </p>
+              <p className="card__text">{summary}</p>
             </Grid.Column>
           </Grid.Row>
 
@@ -59,7 +87,7 @@ const Card = ({ season }) => {
                   name="map marker alternate"
                   color={colorConfig[season][2]}
                 />
-                <span>Banff, Canada</span>
+                <span>{startLocation.description}</span>
               </div>
             </Grid.Column>
             <Grid.Column width="8">
@@ -68,19 +96,24 @@ const Card = ({ season }) => {
                   name="calendar alternate"
                   color={colorConfig[season][2]}
                 />
-                <span>April 2021</span>
+                <span>
+                  {new Date(startDates[0]).toLocaleString('en-us', {
+                    month: 'long',
+                    year: 'numeric'
+                  })}
+                </span>
               </div>
             </Grid.Column>
             <Grid.Column width="8">
               <div className="card__data">
                 <Icon name="flag" color={colorConfig[season][2]} />
-                <span>3 stops</span>
+                <span>{locations.length} stops</span>
               </div>
             </Grid.Column>
             <Grid.Column width="8">
               <div className="card__data">
                 <Icon name="users" color={colorConfig[season][2]} />
-                <span>25 people</span>
+                <span>{maxGroupSize} people</span>
               </div>
             </Grid.Column>
           </Grid.Row>
@@ -93,19 +126,21 @@ const Card = ({ season }) => {
             <Grid.Column width="10">
               <Label as="div" tag size="big">
                 <Icon name="dollar sign" />
-                <span className="card__footer-value">297</span>
+                <span className="card__footer-value">{price}</span>
                 <span className="card__footer-text">per person</span>
               </Label>
               <Label as="div" tag size="big">
                 <Icon name="star" />
-                <span className="card__footer-value">4.9</span>
-                <span className="card__footer-text">rating (21)</span>
+                <span className="card__footer-value">{ratingsAverage}</span>
+                <span className="card__footer-text">
+                  rating ({ratingsQuantity})
+                </span>
               </Label>
             </Grid.Column>
 
             <Grid.Column width="6">
               <CustomBtn
-                linkTo="/tour/test"
+                linkTo={`/tour/${slug}`}
                 classes="custom-btn"
                 rgb={colorConfig[season][1]}
               >
