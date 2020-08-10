69f5bae170732e65213ac38ec75ecb1dfb507a5f
Mostafa Ghafouri <mstghafouri@gmail.com>
2020-08-10 22:01:00 +0430

    Update limit size

--- server/utils/apiFeatures.js
+++ server/utils/apiFeatures.js
@@ -40,7 +40,7 @@ module.exports = class {
 
   paginate() {
     const page = +this.requestQuery.page || 1;
-    const limit = +this.requestQuery.limit || 10;
+    const limit = +this.requestQuery.limit || 6;
     const skip = (page - 1) * limit;
 
     this.query = this.query.skip(skip).limit(limit);
