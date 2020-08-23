04a7bd3fc4d87611b9a6714a5376953d3eba1dad
Mostafa Ghafouri <mstghafouri@gmail.com>
2020-08-23 15:05:31 +0430

    Make 404 page responsive

--- client/src/components/pages/NotFound/index.jsx
+++ client/src/components/pages/NotFound/index.jsx
@@ -8,41 +8,38 @@ const NotFound = () => {
   }, []);
 
   return (
-    <Grid
-      container
-      textAlign="center"
-      className="notFound-page"
-      verticalAlign="middle"
-    >
-      <Grid.Row>
-        <Grid.Column width="8">
-          <Image
-            src={`${process.env.PUBLIC_URL}/img/404.svg`}
-            alt="Not found!"
-            fluid
-          />
-        </Grid.Column>
-        <Grid.Column width="8">
-          <Header size="huge">404</Header>
-          <Header size="large">
-            OH NO! You're lost in&nbsp;
-            <span style={{ color: 'green' }}>Natours</span>!
-          </Header>
-          <Header size="tiny">
-            The page you are looking for does not exist. How you got here is a
-            mystery. But you can click the button below to go back to the
-            homepage.
-          </Header>
-          <CustomBtn
-            linkTo="/"
-            classes="custom-btn d-inline-block"
-            rgb={'40, 180, 133'}
-          >
-            Home
-          </CustomBtn>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+    <div className="notFound-page">
+      <Grid container stackable textAlign="center" verticalAlign="middle">
+        <Grid.Row>
+          <Grid.Column width="8">
+            <Image
+              src={`${process.env.PUBLIC_URL}/img/404.svg`}
+              alt="Not found!"
+              fluid
+            />
+          </Grid.Column>
+          <Grid.Column width="8">
+            <Header size="huge">404</Header>
+            <Header size="large">
+              OH NO! You're lost in&nbsp;
+              <span style={{ color: 'green' }}>Natours</span>!
+            </Header>
+            <Header size="tiny">
+              The page you are looking for does not exist. How you got here is a
+              mystery. But you can click the button below to go back to the
+              homepage.
+            </Header>
+            <CustomBtn
+              linkTo="/"
+              classes="custom-btn d-inline-block"
+              rgb={'40, 180, 133'}
+            >
+              Home
+            </CustomBtn>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
   );
 };
 
