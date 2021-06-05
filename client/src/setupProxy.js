b4b50fc8f2191670e84b8286845448a07c1f4c4e
Mostafa Ghafouri <mstghafouri@gmail.com>
2021-06-05 13:20:52 +0430

    Update setupProxy.js file

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const EXPRESS_HOST = process.env.EXPRESS_HOST || 'localhost';
+
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware('/api/**', { target: 'http://localhost:3007' })
+    createProxyMiddleware('/api/**', { target: `http://${EXPRESS_HOST}:5050` })
   );
 };
