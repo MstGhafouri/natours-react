3a2f55de869f46e358034dec47b1efca9d969896
Mostafa Ghafouri <mstghafouri@gmail.com>
2021-06-11 18:41:15 +0430

    Update stripe.js file

--- client/src/redux/actions/stripe.js
+++ client/src/redux/actions/stripe.js
@@ -2,7 +2,7 @@ import * as types from './types';
 import errorHandler from './errorHandler';
 import natoursApi from '../../api/natoursApi';
 
-const stripe = window.Stripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+const stripe = window?.Stripe?.(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 export const bookTour = tourId => {
   return async dispatch => {
